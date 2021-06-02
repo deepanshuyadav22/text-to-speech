269ded94829445a5fd1fde11608ee8281d51a895
deepanshuyadav22 <59260686+deepanshuyadav22@users.noreply.github.com>
2021-06-02 21:29:52 +0530

    Update Text to Speech.py

--- Text to Speech.py
+++ Text to Speech.py
@@ -8,22 +8,46 @@ NOTE: If the text field is filled and also a text-file is chosen, then this tkin
 
 #Imported Modules and Libraries-------------------------------------------
 
-from tkinter import * #to implement GUI
-from tkinter.filedialog import askopenfilename #to choose a text-file for TTS
-from tkinter import messagebox #to display an alert message after clicking on 'Save as MP3' button
-from langdetect import detect #to detect input language of entered texts or chosen text-file
-from gtts import gTTS #TTS API
-from googletrans import Translator #to speak the given input in the language user want
-from playsound import playsound #to play audio file of TTS
-import os #to delete saved audio files when tkinter-app closes (EXCEPT FILES SAVED WITH 'SAVE AS MP3' BUTTON)
-import emoji #to display heart at bottom right corner
+#to implement GUI
+from tkinter import *
+
+#to choose a text-file for TTS
+from tkinter.filedialog import askopenfilename
+
+#to display an alert messages
+from tkinter import messagebox
+
+#to detect input language of entered texts or chosen text-file
+from langdetect import detect
+
+#TTS API
+from gtts import gTTS
+
+#to speak the given input in the language user want
+from googletrans import Translator
+
+#to play audio file of TTS
+from playsound import playsound
+
+#to delete saved audio files when tkinter-app closes (EXCEPT FILES SAVED WITH 'SAVE AS MP3' BUTTON)
+import os
+
+#to display heart at bottom right corner
+import emoji
 
 #Designing Main Window----------------------------------------------------
 
-w = Tk() #creating main window
-w.title("Text to Speech") #title of main window
-w.geometry("930x600") #size of main window
-w.configure(background='#fff') #background colour of main window
+#creating main window
+w = Tk()
+
+#title of main window
+w.title("Text to Speech")
+
+#size of main window
+w.geometry("930x600")
+
+#background colour of main window
+w.configure(background='#fff')
 
 #Functions Section--------------------------------------------------------
 
@@ -43,28 +67,28 @@ def choose_file(event):
         file_content.append(open(file_name, 'r', encoding='utf-8').read())
 
         #setting states to Normal to write path, name and content of the chosen text-file
-        text_2.config(state = "normal")
-        text_filename.config(state = "normal")
-        text_content.config(state = "normal")
+        entry_displayFilePath.config(state = "normal")
+        entry_displayFilename.config(state = "normal")
+        text_displayFileContent.config(state = "normal")
 
-        text_2.delete(0, END) #removing the line "No file chosen"
-        text_2.insert(END, file_name) #and writing the path of chosen text-file
+        entry_displayFilePath.delete(0, END) #removing the line "No file chosen"
+        entry_displayFilePath.insert(END, file_name) #and writing the path of chosen text-file
 
         #getting the name of chosen text-file
         for i in range(0, len(file_name)):
             if file_name[i] == '/':
                 file_name_ind = i + 1;
 
-        text_filename.delete(0, END) #removing the line "No file chosen"
-        text_filename.insert(END, file_name[file_name_ind:]) #and writing the name of chosen text-file
+        entry_displayFilename.delete(0, END) #removing the line "No file chosen"
+        entry_displayFilename.insert(END, file_name[file_name_ind:]) #and writing the name of chosen text-file
 
-        text_content.delete("1.0", "end") #removing the line "No file chosen"
-        text_content.insert(END, file_content[0]) #and writing the content of chosen text-file
+        text_displayFileContent.delete("1.0", "end") #removing the line "No file chosen"
+        text_displayFileContent.insert(END, file_content[0]) #and writing the content of chosen text-file
 
         #setting states to Readonly & Disabled to prevent manual editing of path, name and content of the chosen text-file
-        text_2.config(state = "readonly")
-        text_filename.config(state = "readonly")
-        text_content.config(state = "disabled")
+        entry_displayFilePath.config(state = "readonly")
+        entry_displayFilename.config(state = "readonly")
+        text_displayFileContent.config(state = "disabled")
     except:
         print(end = "")
 
@@ -98,7 +122,7 @@ def speech_or_save(speech_content, ss):
             tts_count.append(0)
 
             #generating filename for the audio file (naming format - 'Audio-N', where N is a natural number)
-            tts_audio = "../Audio-" + str(len(tts_count)) + ".mp3"
+            tts_audio = "Audio-" + str(len(tts_count)) + ".mp3"
 
             tts.save(tts_audio) #saving the audio
             playsound(tts_audio) #playing the audio
@@ -120,8 +144,10 @@ def speech_or_save(speech_content, ss):
                 if tts_audio not in tts_audio_list:
                     #then save the audio file with generated name
                     tts.save(tts_audio)
+
                     #a message-box to display that the file has been saved
                     messagebox.showinfo("File saved", "The file has been saved with name " + tts_audio)
+
                     break
                 else:
                     i += 1
@@ -130,24 +156,30 @@ def speech_or_save(speech_content, ss):
 
 #function to speak entered texts or content of chosen text-file
 def order_to_speech(event):
-    text_content = str(text_1.get()) #getting entered texts
+    text_displayFileContent = str(entry_inputText.get()) #getting entered texts
 
-    if text_content != "": 
-        speech_or_save(text_content, 0) #if text field is filled
+    if text_displayFileContent != "": 
+        #if text field is filled
+        speech_or_save(text_displayFileContent, 0)
+
     elif len(file_content) > 0:
-        speech_or_save(file_content[0], 0) #if text field is empty but a text-file has been chosen
+        #if text field is empty but a text-file has been chosen
+        speech_or_save(file_content[0], 0)
+
     else:
         #if both the fields are empty
         messagebox.showinfo("Incomplete action!", "Please enter a text or choose a text-file.")
 
 #function to save input as an audio file
 def save_as_mp3(event):
-    text_content = str(text_1.get())
+    text_displayFileContent = str(entry_inputText.get())
 
-    if text_content != "":
-        speech_or_save(text_content, 1)
+    if text_displayFileContent != "":
+        speech_or_save(text_displayFileContent, 1)
+
     elif len(file_content) > 0:
         speech_or_save(file_content[0], 1)
+        
     else:
         #if both the fields are empty
         messagebox.showinfo("Incomplete action!", "Please enter a text or choose a text-file.")
@@ -157,27 +189,27 @@ def reset(event):
     file_content = []
     tts_count = []
 
-    text_2.config(state = "normal")
-    text_filename.config(state = "normal")
-    text_content.config(state = "normal")
+    entry_displayFilePath.config(state = "normal")
+    entry_displayFilename.config(state = "normal")
+    text_displayFileContent.config(state = "normal")
 
-    text_1.delete(0, END)
-    text_2.delete(0, END)
-    text_2.insert(END, "No file chosen")
+    entry_inputText.delete(0, END)
+    entry_displayFilePath.delete(0, END)
+    entry_displayFilePath.insert(END, "No file chosen")
 
     speech_in.set("Default")
 
     speech_speed_dflt.select()
     speech_speed_slow.deselect()
 
-    text_filename.delete(0, END)
-    text_content.delete("1.0", "end")
-    text_filename.insert(END, "No file chosen")
-    text_content.insert(END, "No file chosen")
+    entry_displayFilename.delete(0, END)
+    text_displayFileContent.delete("1.0", "end")
+    entry_displayFilename.insert(END, "No file chosen")
+    text_displayFileContent.insert(END, "No file chosen")
 
-    text_2.config(state = "readonly")
-    text_filename.config(state = "readonly")
-    text_content.config(state = "disabled")
+    entry_displayFilePath.config(state = "readonly")
+    entry_displayFilename.config(state = "readonly")
+    text_displayFileContent.config(state = "disabled")
 
 #Heading Section----------------------------------------------------------
 
@@ -187,50 +219,49 @@ heading.pack()
 
 #Input Section-------------------------------------------------------
 
-lable_1 = Label(w, text="Enter the text:", bg='#fff', font=('segoeui',14))
-lable_2 = Label(w, text="OR", bg='#fff', font=('segoeui',14))
-button_1 = Button(w, text="Choose a text file", width=15, height=1, bg='#077bff', fg='#fff', activebackground='blue', activeforeground='#fff', font=('segoeui',14), bd=0)
+lable_enterText = Label(w, text="Enter the text:", bg='#fff', font=('segoeui',14))
+lable_displayOR = Label(w, text="OR", bg='#fff', font=('segoeui',14))
+button_chooseFile = Button(w, text="Choose a text file", width=15, height=1, bg='#077bff', fg='#fff', activebackground='blue', activeforeground='#fff', font=('segoeui',14), bd=0)
 label_filename = Label(w, text="Content of the chosen text file:", bg='#fff', font=('segoeui',14))
 
-text_1 = Entry(width='43', bg='#f1f1f1', bd='0', font=('calibri',14))
-text_2 = Entry(width='37', bg='#f1f1f1', bd='0', font=('calibri',14))
-text_filename = Entry(width='29', bg='#f1f1f1', bd='0', font=('calibri',14))
-text_content = Text(w, width = '57', height = '12', bg='#f1f1f1', wrap = WORD, bd = '0', font=('calibri',14))
+entry_inputText = Entry(width='43', bg='#f1f1f1', bd='0', font=('calibri',14))
+entry_displayFilePath = Entry(width='37', bg='#f1f1f1', bd='0', font=('calibri',14))
+entry_displayFilename = Entry(width='29', bg='#f1f1f1', bd='0', font=('calibri',14))
+text_displayFileContent = Text(w, width = '57', height = '12', bg='#f1f1f1', wrap = WORD, bd = '0', font=('calibri',14))
 
 #when tkinter-app starts, below three will display "No file chosen"
-text_2.insert(END, "No file chosen")
-text_filename.insert(END, "No file chosen")
-text_content.insert(END, "No file chosen")
+entry_displayFilePath.insert(END, "No file chosen")
+entry_displayFilename.insert(END, "No file chosen")
+text_displayFileContent.insert(END, "No file chosen")
 
 #setting states to Readonly & Disabled to prevent manual editing of path, name and content of the chosen text-file
-text_2.config(state = "readonly")
-text_filename.config(state = "readonly")
-text_content.config(state = "disabled")
+entry_displayFilePath.config(state = "readonly")
+entry_displayFilename.config(state = "readonly")
+text_displayFileContent.config(state = "disabled")
 
-lable_1.pack()
-lable_2.pack()
-button_1.pack()
+lable_enterText.pack()
+lable_displayOR.pack()
+button_chooseFile.pack()
 label_filename.pack()
-text_content.pack()
+text_displayFileContent.pack()
 
-button_1.bind('<Button-1>', choose_file)
+button_chooseFile.bind('<Button-1>', choose_file)
 
-lable_1.place(x = 50, y = 100)
-lable_2.place(x = 100, y = 140)
-button_1.place(x = 50, y = 180)
+lable_enterText.place(x = 50, y = 100)
+lable_displayOR.place(x = 100, y = 140)
+button_chooseFile.place(x = 50, y = 180)
 label_filename.place(x = 50, y = 255)
 
-text_1.place(x = 190, y = 100)
-text_2.place(x = 247, y = 185)
-text_filename.place(x = 330, y = 255)
-text_content.place(x = 50, y = 295)
-
+entry_inputText.place(x = 190, y = 100)
+entry_displayFilePath.place(x = 247, y = 185)
+entry_displayFilename.place(x = 330, y = 255)
+text_displayFileContent.place(x = 50, y = 295)
 
 #Choose the Language for Output Voice-------------------------------------
 
-lable_3 = Label(w, text="Speech in:", bg='#fff', font=('segoeui',14))
-lable_3.pack()
-lable_3.place(x = 675, y = 98)
+label_chooseLang = Label(w, text="Speech in:", bg='#fff', font=('segoeui',14))
+label_chooseLang.pack()
+label_chooseLang.place(x = 675, y = 98)
 
 langs = ['Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Azerbaijani', 'Basque', 'Belarusian', 'Bengali', 'Bosnian', 'Bulgarian', 'Catalan', 'Cebuano', 'Chichewa', 'Chinese (simplified)', 'Chinese (traditional)', 'Corsican', 'Croatian', 'Czech', 'Danish', 'Default', 'Dutch', 'English', 'Esperanto', 'Estonian', 'Filipino', 'Finnish', 'French', 'Frisian', 'Galician', 'Georgian', 'German', 'Greek', 'Gujarati', 'Haitian creole', 'Hausa', 'Hawaiian', 'Hebrew', 'Hebrew', 'Hindi', 'Hmong', 'Hungarian', 'Icelandic', 'Igbo', 'Indonesian', 'Irish', 'Italian', 'Japanese', 'Javanese', 'Kannada', 'Kazakh', 'Khmer', 'Korean', 'Kurdish (kurmanji)', 'Kyrgyz', 'Lao', 'Latin', 'Latvian', 'Lithuanian', 'Luxembourgish', 'Macedonian', 'Malagasy', 'Malay', 'Malayalam', 'Maltese', 'Maori', 'Marathi', 'Mongolian', 'Myanmar (burmese)', 'Nepali', 'Norwegian', 'Odia', 'Pashto', 'Persian', 'Polish', 'Portuguese', 'Punjabi', 'Romanian', 'Russian', 'Samoan', 'Scots gaelic', 'Serbian', 'Sesotho', 'Shona', 'Sindhi', 'Sinhala', 'Slovak', 'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili', 'Swedish', 'Tajik', 'Tamil', 'Telugu', 'Thai', 'Turkish', 'Ukrainian', 'Urdu', 'Uyghur', 'Uzbek', 'Vietnamese', 'Welsh', 'Xhosa', 'Yiddish', 'Yoruba', 'Zulu']
 
@@ -247,9 +278,9 @@ lang_list.place(x = 790, y = 98)
 
 #Speed Control Section for Output Voice-----------------------------------
 
-lable_4 = Label(w, text="Speed of speech:", bg='#fff', font=('segoeui',14))
-lable_4.pack()
-lable_4.place(x = 675, y = 180)
+label_chooseRate = Label(w, text="Speed of speech:", bg='#fff', font=('segoeui',14))
+label_chooseRate.pack()
+label_chooseRate.place(x = 675, y = 180)
 
 speech_speed = IntVar()
 
@@ -268,27 +299,27 @@ speech_speed_slow.place(x = 765, y = 220)
 
 #Buttons to Do Tasks------------------------------------------------------
 
-button_2 = Button(w, text="Order to Speech", width=15, height=2, bg='#077bff', fg='#fff', activebackground='blue', activeforeground='#fff', font=('centurygothic',14), bd=0)
-button_3 = Button(w, text="Save as MP3", width=15, height=2, bg='#077bff', fg='#fff', activebackground='blue', activeforeground='#fff', font=('centurygothic',14), bd=0)
-button_4 = Button(w, text="Reset", width=15, height=2, bg='red', fg='#fff', activebackground='darkred', activeforeground='#fff', font=('centurygothic',14), bd=0)
+button_OrderToSpeech = Button(w, text="Order to Speech", width=15, height=2, bg='#077bff', fg='#fff', activebackground='blue', activeforeground='#fff', font=('centurygothic',14), bd=0)
+button_SaveAsMP3 = Button(w, text="Save as MP3", width=15, height=2, bg='#077bff', fg='#fff', activebackground='blue', activeforeground='#fff', font=('centurygothic',14), bd=0)
+button_Reset = Button(w, text="Reset", width=15, height=2, bg='red', fg='#fff', activebackground='darkred', activeforeground='#fff', font=('centurygothic',14), bd=0)
 
-button_2.pack()
-button_3.pack()
-button_4.pack()
+button_OrderToSpeech.pack()
+button_SaveAsMP3.pack()
+button_Reset.pack()
 
-button_2.bind('<Button-1>', order_to_speech)
-button_3.bind('<Button-1>', save_as_mp3)
-button_4.bind('<Button-1>', reset)
+button_OrderToSpeech.bind('<Button-1>', order_to_speech)
+button_SaveAsMP3.bind('<Button-1>', save_as_mp3)
+button_Reset.bind('<Button-1>', reset)
 
-button_2.place(x = 675, y = 310)
-button_3.place(x = 675, y = 390)
-button_4.place(x = 675, y = 470)
+button_OrderToSpeech.place(x = 675, y = 310)
+button_SaveAsMP3.place(x = 675, y = 390)
+button_Reset.place(x = 675, y = 470)
 
 #Developers---------------------------------------------------------------
 
-lable_4 = Label(w, text=emoji.emojize("Created with :red_heart: by AMD"), bg='#fff', font=('verdana',12))
-lable_4.pack()
-lable_4.place(x = 725, y = 570)
+label_developer = Label(w, text=emoji.emojize("Created with :red_heart: by Deepanshu"), bg='#fff', font=('verdana',12))
+label_developer.pack()
+label_developer.place(x = 670, y = 570)
 
 #-------------------------------------------------------------------------
 
